77e94e1140bd3bb3408a760703ee2457c79788e5
Zlatko Petrov <zpetrov@sevone.com>
2016-01-04 13:35:29 +0200

    index

---
+++ src/AppBundle/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class IndexController extends Controller
+{
+    public function indexAction()
+    {
+        if(!$this->getUser())
+            $this->redirect($this->generateUrl('fos_user_security_login'));
+
+        return $this->render('AppBundle:IndexController:index.html.twig', array(
+            // ...
+        ));
+    }
+
+}
